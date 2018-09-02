fb66fe86568fad7940052d1ba07df2a46f4adfa0
ericstaal <eric.staal@gmail.com>
2018-09-02 07:04:58 +0200

    Solve connection issues

--- plugin/LG/plugin.py
+++ plugin/LG/plugin.py
@@ -9,9 +9,11 @@
 # 1.0.2   31-07-2017  Updated with new API
 # 1.0.3   06-08-2017  Added mute button
 # 1.0.4   25-05-2018  Removed bash ping used internal ICMP
+# 1.0.5   06-08-2018  Update logging
+# 1.0.6   10-08-2018  Fix connection issues
 
 """
-<plugin key="LGtv" name="LG TV" author="elgringo" version="1.0.4" externallink="https://github.com/ericstaal/domoticz/blob/master/">
+<plugin key="LGtv" name="LG TV" author="elgringo" version="1.0.6" externallink="https://github.com/ericstaal/domoticz/blob/master/">
   <params>
     <param field="Address" label="IP address" width="200px" required="true" default="192.168.13.15"/>
     <param field="Port" label="Port" width="30px" required="false" default="8080"/>
@@ -22,6 +24,18 @@
         <option label="20" value="20" default="true"/>
         <option label="25" value="25"/>
         <option label="30" value="30"/>
+      </options>
+    </param>
+    <param field="Mode3" label="max queued commands" width="50px" required="true">
+      <options>
+        <option label="1" value="1" default="true"/>
+        <option label="2" value="2"/>
+        <option label="3" value="3" />
+        <option label="4" value="4"/>
+        <option label="5" value="5"/>
+        <option label="10" value="10"/>
+        <option label="20" value="20"/>
+        <option label="50" value="50"/>
       </options>
     </param>
     <param field="Mode4" label="Pairing key" width="200px" default="" /> 
@@ -83,6 +97,7 @@ class BasePlugin:
   queuedCommands = []
   sessionState = 0 # 0 = pairing, 1 = session, 2 = command
   session = None # session ID, updated every heartbeat???
+  maxQueued = 1
   
   # dictionary with all codes
   LGCodes = {
@@ -173,12 +188,14 @@ class BasePlugin:
     # Check connection and connect none
     isConnected = False
     
-    if not self.connection is None:
-      if self.connection.Connected():
-        isConnected = True
-      else:
-        if (not self.connection.Connecting()) and (not checkonly):
-          self.connection.Connect()
+    if self.connection is None:
+      self.connection = Domoticz.Connection(Name="LG_TCP", Transport="TCP/IP", Protocol="HTTP", Address=Parameters["Address"], Port=Parameters["Port"])
+    
+    if self.connection.Connected():
+      isConnected = True
+    else:
+      if (not self.connection.Connecting()) and (not checkonly):
+        self.connection.Connect()
     
     return isConnected
     
@@ -188,24 +205,29 @@ class BasePlugin:
     try:
       self.logLevel = int(Parameters["Mode6"])
     except:
-      self.LogError("Debuglevel '"+Parameters["Mode6"]+"' is not an integer")
+      self.Log("Debuglevel '"+Parameters["Mode6"]+"' is not an integer", 1, 3)
       
     if self.logLevel == 10:
       Domoticz.Debugging(1)
-    self.LogMessage("onStart called", 9)
+    self.Log("onStart called", 9, 1)
     
-    self.connection = Domoticz.Connection(Name="LG_TCP", Transport="TCP/IP", Protocol="HTTP", Address=Parameters["Address"], Port=Parameters["Port"])
             
     if (self.regexIp.match(Parameters["Address"] )):
       self.ip = Parameters["Address"]
     else:
-      self.LogError("'"+Parameters["Address"]+"' is not a valid IP address." )
+      self.Log("'"+Parameters["Address"]+"' is not a valid IP address.", 1, 3 )
     
     try:
       self.port = int(Parameters["Port"])
     except Exception as e:
-      self.LogError("Port is not a number: "+ Parameters["Port"])
+      self.Log("Port is not a number: "+ Parameters["Port"], 1, 3)
     
+    
+    try:
+      self.maxQueued = int(Parameters["Mode3"])
+    except Exception as e:
+      self.Log("'"+Parameters["Mode3"]+"' is not a number.", 1, 3 )
+      
     try: 
       Domoticz.Heartbeat(int(Parameters["Mode2"]))
     except:
@@ -215,14 +237,14 @@ class BasePlugin:
     
     
     if self.useBashPing:
-      self.LogMessage ( "Pinging with bash, temp file: " + self.tempFile, 1)
+      self.Log ( "Pinging with bash, temp file: " + self.tempFile, 1, 1)
     
       if (os.path.isfile(self.tempFile)):
         subprocess.call('sudo chmod +wr '+ self.tempFile , shell=True)
         os.remove(self.tempFile)
         
     else:
-      self.LogMessage ( "Pinging with internal ICMP connection", 1)    
+      self.Log ( "Pinging with internal ICMP connection", 1, 1)    
     
    
     #ICONS
@@ -264,7 +286,7 @@ class BasePlugin:
     return
 
   def onStop(self):
-    self.LogMessage("onStop called", 4)
+    self.Log("onStop called", 4,1)
     if (self.icmpConnection != None):
       self.icmpConnection.Disconnect()
       self.icmpConnection = None
@@ -273,12 +295,12 @@ class BasePlugin:
 
   def onConnect(self, Connection, Status, Description):
     if (Status == 0):
-      self.LogMessage("Connected successfully to: "+Connection.Address+":"+Connection.Port, 5)
+      self.Log("Connected successfully to: "+Connection.Address+":"+Connection.Port, 4, 2)
       
       # if connect we must send data
       # depending on if a key/session/command is known
       if (len(self.key) <= 2):
-        self.LogMessage("Pairing key is unknown. Request pairing key (shown on TV)", 1)
+        self.Log("Pairing key is unknown. Request pairing key (shown on TV)", 1, 2)
         reqKey = "<!--?xml version=\"1.0\" encoding=\"utf-8\"?--><auth><type>AuthKeyReq</type></auth>"
         self.sessionState = 0 
         self.sendMessage(Message=reqKey, URL="/hdcp/api/auth")
@@ -293,14 +315,15 @@ class BasePlugin:
           items = len(self.queuedCommands)
           if items > 0:
             cmd = self.queuedCommands.pop(0)
-            self.LogMessage("Sending command '" + cmd +"', still "+str(items-1)+" command in queue", 5)
+            self.Log("Sending command '" + cmd +"', still "+str(items-1)+" command in queue", 5, 1)
             
             cmdText = '<?xml version="1.0" encoding="utf-8"?><command><session>'+self.session+'</session><name>HandleKeyInput</name><value>'+str(self.LGCodes[cmd])+'</value></command>'
             self.sessionState = 2
             self.sendMessage(Message=cmdText, URL="/hdcp/api/dtv_wifirc") 
+            
                   
     else:
-      self.LogMessage("Failed to connect ("+str(Status)+") to: "+Connection.Address+":"+Connection.Port+" with error: "+Description, 4)
+      self.Log("Failed to connect ("+str(Status)+") to: "+Connection.Address+":"+Connection.Port+" with error: "+Description, 4, 2)
       self.queuedCommands.clear() # clear send commands
       self.session = None
         
@@ -318,19 +341,19 @@ class BasePlugin:
           try:
             tree = etree.XML(Data['Data'])
             self.session = tree.find('session').text
-            self.LogMessage("Session ID: "+self.session , 7)
+            self.Log("Session ID: "+self.session , 7, 1)
           except:
             pass
     else: # PING  
       if not self.useBashPing:
         if isinstance(Data, dict) and (Data["Status"] == 0): 
-          self.LogMessage("Ping reply [ms]: "+str(Data["ElapsedMs"]) , 6)
+          self.Log("Ping reply [ms]: "+str(Data["ElapsedMs"]) , 6, 1)
           if not self.lastState: # last state was offline
             Devices[1].Update( 1, "On") # update
             self.lastState = True 
         else:
           # not alive
-          self.LogMessage("Not alive", 6)
+          self.Log("Not alive", 6, 1)
           if self.lastState:
             Devices[1].Update( 0, "Off")
             self.lastState = False
@@ -342,55 +365,57 @@ class BasePlugin:
     return
 
   def onCommand(self, Unit, Command, Level, Hue):
-    self.LogMessage("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level)+", Hue: " + str(Hue), 8)
+    self.Log("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level)+", Hue: " + str(Hue), 8, 1)
     
     CommandStr = str(Command)
       
-    if (Unit == 1):
-      #if ( CommandStr == "Off"):
-      self.queuedCommands.append("power_off")
-      #Devices[1].Update( 0, "Off")
-    if (Unit == 2):
-      self.queuedCommands.append("volume_up")
-    if (Unit == 3):
-      self.queuedCommands.append("volume_down")
-    if (Unit == 4):
-      self.queuedCommands.append("channel_up")
-    if (Unit == 5):
-      self.queuedCommands.append("channel_down")
-    if (Unit == 6):
-      self.queuedCommands.append("hdmi1")
-    if (Unit == 7):
-      self.queuedCommands.append("tv_radio")
-    if (Unit == 8):
-      self.queuedCommands.append("mute")
-    if (Unit == 9):
-      self.queuedCommands.append("exit")
-    
-    if len(self.queuedCommands) > 0:
+    if self.maxQueued > len(self.queuedCommands):
+      if (Unit == 1):
+        self.queuedCommands.append("power_off")
+      if (Unit == 2):
+        self.queuedCommands.append("volume_up")
+      if (Unit == 3):
+        self.queuedCommands.append("volume_down")
+      if (Unit == 4):
+        self.queuedCommands.append("channel_up")
+      if (Unit == 5):
+        self.queuedCommands.append("channel_down")
+      if (Unit == 6):
+        self.queuedCommands.append("hdmi1")
+      if (Unit == 7):
+        self.queuedCommands.append("tv_radio")
+      if (Unit == 8):
+        self.queuedCommands.append("mute")
+      if (Unit == 9):
+        self.queuedCommands.append("exit")
+      
       self.DumpVariable(self.queuedCommands, "queuedCommands")
-      self.checkConnection()
+      if len(self.queuedCommands) == 1:
+        self.checkConnection()
+    else:
+      self.Log("Still "+ str(len(self.queuedCommands))+ " commands queued, drop this command" , 6, 1)
       
     return
 
   def onNotification(self, Name, Subject, Text, Status, Priority, Sound, ImageFile):
-    self.LogMessage("onNotification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(Priority) + "," + Sound + "," + ImageFile, 8)
+    self.Log("onNotification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(Priority) + "," + Sound + "," + ImageFile, 8, 1)
     
     return
 
   def onDisconnect(self, Connection):
+    
     items = len(self.queuedCommands)
-    self.LogMessage("onDisconnect "+Connection.Address+":"+Connection.Port+", still "+ str(items)+" in the queue", 7)
-   
+    self.Log("onDisconnect "+Connection.Address+":"+Connection.Port+", still "+ str(items)+" in the queue", 4, 2)
+    
     # if there are still command continue
     if (items > 0) and (len(self.key) > 2):
-      self.DumpVariable(self.queuedCommands, "queuedCommands", Level=7)
+      self.DumpVariable(self.queuedCommands, "queuedCommands", Level=6)
       self.checkConnection()
         
     return
 
   def onHeartbeat(self):
-    self.LogMessage("onHeartbeat called", 9)
+    self.Log("onHeartbeat called", 9,1)
    
     # check if TV is online
     if self.useBashPing:
@@ -407,10 +432,10 @@ class BasePlugin:
           file.close()
           
           online = len(self.regexOnline.findall(text)) > 0
-          self.LogMessage("Is device online: "+ str(online), 7)
+          self.Log("Is device online: "+ str(online), 7, 1)
           os.remove(self.tempFile)
         except Exception as e:
-          self.LogError("Failed reading '"+self.tempFile+"' : "+ str(e))
+          self.Log("Failed reading '"+self.tempFile+"' : "+ str(e), 1, 3)
           
         if online: # device is online
           if not self.lastState: # last state was offline
@@ -424,15 +449,14 @@ class BasePlugin:
             
       command = 'ping -c 1 -n -s 1 -q '+ self.ip  + ' > '+self.tempFile+' &'
       subprocess.call(command , shell=True)
-      self.LogMessage(command, 9)
+      self.Log(command, 9, 1)
     else:
       if (self.icmpConnection == None):
         self.icmpConnection = Domoticz.Connection(Name="LG_ICMP", Transport="ICMP/IP", Protocol="ICMP", Address=Parameters["Address"])
         self.icmpConnection.Listen()
       else:
-        self.LogMessage("onHeartbeat called, send PING message", 6)
-        self.icmpConnection.Send("Domoticz")
-    
+        self.Log("onHeartbeat called, send icmp PING message", 6, 1)
+        self.icmpConnection.Send("Domoticz")    
       
     return
 
@@ -442,7 +466,8 @@ class BasePlugin:
     Headers['Host']=self.connection.Address+":"+self.connection.Port
     data = {"Verb":Verb, "URL":URL, "Headers": Headers, 'Data': Message}
     self.DumpVariable(data, "Data to send", Level=8, BytesAsStr = True)
-    self.connection.Send(data)
+    if self.connection.Connected:
+      self.connection.Send(data)
 
     return
     
@@ -465,19 +490,19 @@ class BasePlugin:
         sValue = str(sValue1)+";"+str(sValue2)
         
       if (Devices[Unit].nValue != nValue) or (Devices[Unit].sValue != sValue):
-        self.LogMessage("Update ["+Devices[Unit].Name+"] from: ('"+str(Devices[Unit].nValue)+":'"+str(Devices[Unit].sValue )+"') to: ("+str(nValue)+":'"+str(sValue)+"')", 5)
+        self.Log("Update ["+Devices[Unit].Name+"] from: ('"+str(Devices[Unit].nValue)+":'"+str(Devices[Unit].sValue )+"') to: ("+str(nValue)+":'"+str(sValue)+"')", 5, 1)
         Devices[Unit].Update(nValue, sValue)
     return
    
   def DumpDeviceToLog(self,Unit):
-    self.LogMessage(str(Devices[Unit].ID)+":"+Devices[Unit].Name+", (n:"+str(Devices[Unit].nValue)+", s:"+Devices[Unit].sValue+", Sgl:"+str(Devices[Unit].SignalLevel)+", bl:"+str(Devices[Unit].BatteryLevel)+", img:"+ str(Devices[Unit].Image)+", typ:"+ str(Devices[Unit].Type)+", styp:"+ str(Devices[Unit].SubType)+")", 6)
+    self.Log(str(Devices[Unit].ID)+":"+Devices[Unit].Name+", (n:"+str(Devices[Unit].nValue)+", s:"+Devices[Unit].sValue+", Sgl:"+str(Devices[Unit].SignalLevel)+", bl:"+str(Devices[Unit].BatteryLevel)+", img:"+ str(Devices[Unit].Image)+", typ:"+ str(Devices[Unit].Type)+", styp:"+ str(Devices[Unit].SubType)+")", 6, 1)
     return   
     
   def DumpConfigToLog(self):
     for x in Parameters:
       if Parameters[x] != "":
-        self.LogMessage( "'" + x + "':'" + str(Parameters[x]) + "'", 7)
-    self.LogMessage("Device count: " + str(len(Devices)), 6)
+        self.Log( "'" + x + "':'" + str(Parameters[x]) + "'", 7, 1)
+    self.Log("Device count: " + str(len(Devices)), 6, 1)
     for x in Devices:
       self.DumpDeviceToLog(x)
     return
@@ -486,7 +511,7 @@ class BasePlugin:
     if self.logLevel >= Level:
       Prefix = str(Prefix)
       if isinstance(Item, dict):
-        self.LogMessage(Prefix + str(Varname) + " ("+type(Item).__name__+"["+str(len(Item))+"]): ", Level)
+        self.Log(Prefix + str(Varname) + " ("+type(Item).__name__+"["+str(len(Item))+"]): ", Level, 1)
         
         if len(Prefix) < 3:
           Prefix = "--> "
@@ -505,12 +530,12 @@ class BasePlugin:
         else:
           txt = "[ " 
           for b in Item:
-            txt += str(hex(b))+" "
+            txt += '0x{:02X} '.format(b)
           txt +=  "]"
         
-        self.LogMessage(Prefix + str(Varname) + " ("+type(Item).__name__+"["+str(len(Item))+"]): " + txt, Level)
+        self.Log(Prefix + str(Varname) + " ("+type(Item).__name__+"["+str(len(Item))+"]): " + txt, Level, 1)
       elif isinstance(Item, (tuple, list)):
-        self.LogMessage(Prefix + str(Varname) + " ("+type(Item).__name__+"["+str(len(Item))+"]): ", Level)
+        self.Log(Prefix + str(Varname) + " ("+type(Item).__name__+"["+str(len(Item))+"]): ", Level, 1)
         
         if len(Prefix) < 3:
           Prefix = "--> "
@@ -523,33 +548,29 @@ class BasePlugin:
           idx=idx+1
 
       elif isinstance(Item, str):
-        self.LogMessage(Prefix + str(Varname) + " ("+type(Item).__name__+"["+str(len(Item))+"]): '"+Item+"'", Level)
+        self.Log(Prefix + str(Varname) + " ("+type(Item).__name__+"["+str(len(Item))+"]): '"+Item+"'", Level, 1)
       else:
-        self.LogMessage(Prefix + str(Varname) + " ("+type(Item).__name__+"): "+str(Item), Level)
+        self.Log(Prefix + str(Varname) + " ("+type(Item).__name__+"): "+str(Item), Level, 1)
            
     return
 
-  def LogMessage(self, Message, Level):
-    if Level > 0:
-      if self.logLevel >= Level:
-        if self.logLevel >= 10:
-          Domoticz.Debug(Message)
-        else:
-          Domoticz.Log(Message)
-    elif (Level < 0) or (Level > 10):
-      Domoticz.Error(Message)
-      
-    return
+  def Log(self, Message, Level, Type):
+    # Message = string, Level [0-10], Type [1=Normal, 2=Status, 3=Error]
+    if self.logLevel >= Level:
+      if Type == 2:
+        Domoticz.Status(Message)
+      elif Type == 3:
+        Domoticz.Error(Message)
+      else:
+        Domoticz.Log(Message)
     
-  def LogError(self, Message):
-    self.LogMessage(Message, -1)
-    return  
+    return
     
   def stringToBase64(self, s):
     return base64.b64encode(s.encode('utf-8')).decode("utf-8")
 
   def base64ToString(self, b):
-    return base64.b64decode(b).decode('utf-8')      
+    return base64.b64decode(b).decode('utf-8')  
     
 ####################### Global functions for plugin #######################
 global _plugin
@@ -586,3 +607,4 @@ def onDisconnect(Connection):
 def onHeartbeat():
   global _plugin
   _plugin.onHeartbeat()
+
